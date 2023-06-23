3b509bde6d7bff0d221c9ebd8d7ce744af5e9eea
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-06-23 10:41:08 -0300

    Created entity user

--- src/main/java/com/marcotettamanti/userauthentication/UserAuthenticationApplication.java
+++ src/main/java/com/marcotettamanti/userauthentication/UserAuthenticationApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserAuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserAuthenticationApplication.class, args);
+		SpringApplication.run(UserAuthenticationApplication.class, args);	
 	}
-
 }
--- src/main/java/com/marcotettamanti/userauthentication/model/entities/User.java
+++ src/main/java/com/marcotettamanti/userauthentication/model/entities/User.java
@@ -5,15 +5,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_user")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
   
   @Id
+  @EqualsAndHashCode.Include
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
+  private String user;
   private String email;
   private String password;
 }
